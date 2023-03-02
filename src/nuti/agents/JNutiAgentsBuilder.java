5ff91c9af6ab456eb225f77f9e513006d9ed9e14
JulianBenitez <julian.benitezg99@gmail.com>
2023-03-01 21:57:42 -0500

    Init project
    Co-authored-by: Julian Alvarez <julian36alvarez@gmail.com>

---
+++ src/nuti/agents/JNutiAgentsBuilder.java
@@ -0,0 +1,40 @@
+package nuti.agents;
+
+import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
+import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.RandomCartesianAdder;
+import repast.simphony.space.grid.BouncyBorders;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.SimpleGridAdder;
+
+public class JNutiAgentsBuilder implements ContextBuilder<Object> {
+
+	@Override
+	public Context build(Context<Object> context) {
+		context.setId("nuti-agents");
+		
+		// Grid y Space tienen 4000 que representa 4km
+		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder
+				.createContinuousSpaceFactory(null);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace(
+				"space", context, new RandomCartesianAdder<Object>(),
+				new repast.simphony.space.continuous.BouncyBorders(), 
+				4000, 4000);
+
+		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
+		Grid<Object> grid = gridFactory.createGrid("grid", context,
+				new GridBuilderParameters<Object>(new BouncyBorders(),
+						new SimpleGridAdder<Object>(), true, 
+						4000, 4000
+						));
+		
+		return context;
+	}
+
+}
